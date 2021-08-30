1cf0dcb4bdd40df3c06812759d37bd63adc33aef
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-08-30 22:58:14 +0300

    added route to home /, /product:id , /cart

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <main>
+        <Switch>
+          <Route exact path="/" component={HomeScreen} />
+          <Route exact path="/product/:id" component={ProductScreen} />
+          <Route exact path="/cart" component={CartScreen} />
+
+      
+        </Switch>
+      </main>
     </div>
   );
 }
