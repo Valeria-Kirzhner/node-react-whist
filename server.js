181dfd1cedb3eb49da704deb3f25fed5f80276ca
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-08-30 19:20:57 +0300

    created connection with mongoose

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 require("dotenv").config();
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB..."));
+
+
+  
 
 const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => console.log(`Server run on port ${PORT}`));
