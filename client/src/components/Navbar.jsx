f6e889fd24ab013ce8ad8301625e57fb31b4211a
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-08-30 22:43:31 +0300

    created Navbar, BackDrop, SideDrawer components

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            {/* Logo */}
+        <div className="navbar-logo">
+            <h2>Whist Cards</h2>
+        </div>
+            {/* Links */}
+
+            {/* Hamburger */}
+
+        </nav>
+     );
+}
+ 
+export default Navbar;
