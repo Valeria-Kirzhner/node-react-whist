1c136ed79acdac964e7e329bec3259f6831ae15c
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-08-30 19:33:52 +0300

    created product model and passed to mongoose

---
+++ models/Product.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+
+      title: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 30,
+      },
+    description: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 1024,
+      },
+      price: {
+          type: Number,
+          required: true
+      },
+      imageUrl: {
+        type: String,
+        required: true,
+        minlength: 11,
+        maxlength: 1024,
+      },
+});
+// here I telling to mongoose how he should recieve the product card, then mongo will call the table as 'product + s'.
+const Product = mongoose.model('product', productSchema);
+
+module.exports = Product;
