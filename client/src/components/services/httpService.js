7bc770b40243bcad46818b5f26ca99fe63bf9d7e
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-08-31 09:48:41 +0300

    added to httpService all axios request options shortcut andinterceptor on responses

---
+++ client/src/components/services/httpService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+
+axios.defaults.baseURL = "";
+
+axios.interceptors.response.use(null, (error) => {
+    const expectedError = error.response && error.response.status >= 403;
+    if (expectedError) toast.error("An unexpected error occurrred.");
+    return Promise.reject(error);
+  });
+
+const http = {
+    get: axios.get,
+    post: axios.post,
+    put: axios.put,
+    patch: axios.patch,
+    delete: axios.delete,
+  };
+  export default http;
