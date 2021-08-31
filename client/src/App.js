8c5c70cf039245efe2dbacfe7bcb880f96b95f3d
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-08-31 15:26:44 +0300

    created AdminHome component that will render each product

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-
 import React from "react";
 import './App.css';
 import Navbar from "./components/common/Navbar";
 import AddProduct from "./components/admin/AddProduct";
 import Home from "./components/Home";
+import AdminHome from "./components/admin/AdminHome";
 import Product from "./components/Product";
 import { Switch, Route } from "react-router-dom";
 
@@ -18,6 +18,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route path="/product/add" component={AddProduct}/>
           <Route exact path="/product" component={Product}/>
+          <Route exact path="/admin" component={AdminHome}/>
           </Switch>
         </main>
     </React.Fragment>
---
+++ client/src/components/admin/AdminHome.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import PageHeader from "../common/PageHeader";
+import productService from "../services/productService";
+import Product from "./Product";
+
+class AdminHome extends Component {
+
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const { data } = await productService.allProducts();
+    if (data.length > 0) this.setState({ products: data });
+    console.log(this.state.products);
+  }
+
+  render() {
+
+      const { products } = this.state;
+
+    return (
+
+      <div className="container">
+          <PageHeader titleText="All Products" />     
+            <div className="row">
+          { products.length > 0 &&
+            products.map((product) => (
+              <Product
+                key={product._id}
+                product={product}
+              />
+            ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AdminHome;
