628dec72f88ceb409840e3fd11a58470ecf6b067
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-08-31 09:43:18 +0300

    added style to Navbar and links

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
+
+import React from "react";
 import './App.css';
+import Navbar from "./components/common/Navbar";
+import Home from "./components/Home";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-     Hello
-    </div>
+    <React.Fragment>
+       <header>
+          <Navbar />
+        </header>
+        <main>
+          <Switch >
+          <Route exact path="/" component={Home} />
+          </Switch>
+        </main>
+    </React.Fragment>
   );
 }
 
