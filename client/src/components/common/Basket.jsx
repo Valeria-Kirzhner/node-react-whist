e1315b221000ca8f474ae6d234a04b956da1700f
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-09-02 19:00:38 +0300

    added a render of the item price inside the cart and the quantety.

--- client/src/components/common/Basket.jsx
+++ client/src/components/common/Basket.jsx
@@ -4,47 +4,44 @@ const Basket =({cartItems , onAddToCart , onRemoveFromCart})=> {
 
 const [ show, setShow ] = useState(false);
 
-      
-
-        return (  
+    return (  
             
      <div className="container">
         <div className="row">
-             <div className="row">
-                 <div className="col align-self-start"></div>
-                    <div className="col align-self-center"></div>
+            <div className="col align-self-start"></div>
+                <div className="col align-self-center"></div>
+
                         <div className="col align-self-end"> 
-                        <button onClick={ () => setShow(!show)}> <img src={"https://icon-library.com/images/cart-icon/cart-icon-16.jpg"} alt="cart" style={{ width: "50px", height:"50px"}}  /></button>
-                        
+                        <button onClick={ () => setShow(!show)}> <img src={"https://icon-library.com/images/cart-icon/cart-icon-16.jpg"} alt="cart" style={{ width: "50px", height:"50px"}}  /></button>       
                         {
                             show ?
                         
-                        <div className="card" style={{width: "18rem" }}>
-                        <div className="card-header"> </div>
+                        <div className="card" style={{width: "18rem" , minHeight: "300px"}}>
+                        <h4 className="" >Cart items </h4>
+    
+                        <div>{cartItems.length === 0 &&(<div >The cart is empty</div>)}</div>
 
-                      
-                        {cartItems.length === 0 && <div className="card-header" >Cart is empty</div>}
-                            <ul className="list-group list-group-flush" >      
-                                { cartItems.map(( item ) => 
-                                    <div  key={ item._id } >
-                                        <li className="list-group-item">{item.title}
-                                            <div className=".col-md-3 .ml-md-auto">
-                                                <button onClick={() => onAddToCart( item ) } className="btn ">
+                                { cartItems.map(( item ) => // if cartItems !===0 
+                                    <div key={ item._id } >
+                                        <div className="list-group-item">{item.title}
+                                            <div className="col-md-3 ml-md-auto">
+                                                <button onClick={() => onAddToCart( item ) } className="btn">
                                                         +
                                                 </button>
-                                                <button onClick={() => onRemoveFromCart( item )} className="btn ">
+                                                <button onClick={() => onRemoveFromCart( item )} className="btn">
                                                         -
                                                 </button>
                                             </div> 
-                                        </li>
+                                            <div>{item.qty} X ${item.price}</div>
+                                        </div>
                                     </div>  
                                 )}
-                            </ul>
+
+                           
                      </div>
 
                               : null  }
                 </div>
-            </div>
         </div>
     </div>
 
--- client/src/components/product.jsx
+++ client/src/components/product.jsx
@@ -1,28 +1,24 @@
 import React from 'react';
-//import { useCart } from 'react-use-cart';
 
 const Product = ({ product , onAddToCart }) => {
 
-    //const { addItem } = useCart();
-   // const { onAddToCart } = this.props;
-
     return ( 
         <div className="col-md-6 col-lg-4 mt-3">
-        <div className="container">
-            <div className="card" style={{width: "18rem"}}>
-                <img className="card-img-top" width="100" src={product.imageUrl} alt={product.title} />
-                    <div className="card-body">
+            <div className="container">
+                <div className="card" style={{width: "18rem"}}>
+                    <img className="card-img-top" width="100" src={product.imageUrl} alt={product.title} />
+                        <div className="card-body">
                             <h5 className="card-title">{product.title}</h5>
                             <p className="card-text">{product.description}</p>
                         </div>
                             <ul className="list-group list-group-flush">
                                 <li className="list-group-item">{product.price} $</li>
                             </ul>
-                    <div className="card-body">  
-                        < button onClick={() => onAddToCart( product )} className="btn btn-primary" style={{margin: "left 20"}}>Add to Cart</button>
-                    </div>
+                        <div className="card-body">  
+                            < button onClick={() => onAddToCart( product )} className="btn btn-primary" style={{margin: "left 20"}}>Add to Cart</button>
+                        </div>
+                </div>
             </div>
-        </div>
         </div>
 
      );
