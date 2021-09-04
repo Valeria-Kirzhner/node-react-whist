7aae5c2f690b6822d414beee013e1ce67adcbc43
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-09-05 01:13:26 +0300

    added card that render the stats of 5 unique sold products

--- client/src/components/stats/StatHome.jsx
+++ client/src/components/stats/StatHome.jsx
@@ -7,27 +7,31 @@ const StatHome = () => {
 
     const [ topProducts,setTopProducts] = useState([]);
   
+
+
     useEffect(() => {
     
       const fetchData = async () => {
+
         const { data } = await statsService.getStats();
         if (data.length > 0)  setTopProducts(data);
+
       }
-      
       fetchData();
-      console.log(topProducts);
-    }, [] ); 
+    }, [] );     
   
 
     return ( 
         <div className="container">
              <PageHeader titleText="All Products" />     
                  <div className="row">
+                {/* Top products */}
+
                     <div className="col-md-6 col-lg-4 mt-3">
                         <div className="card" style={{width: "18rem"}}>
                             <div className="card-header">
                                 <h5>
-                                Top 5 most sold Products
+                                Top 5 sold Products
                                 </h5>
                             </div>
                             <ul className="list-group list-group-flush">
@@ -50,6 +54,38 @@ const StatHome = () => {
                             </ul>
                         </div>
                     </div>
+
+                    {/*Top orders*/}
+                    <div className="col-md-6 col-lg-4 mt-3">
+                        <div className="card" style={{width: "18rem"}}>
+                            <div className="card-header">
+                                <h5>
+                                Top 5 unique sel
+                                </h5>
+                            </div>
+                            <ul className="list-group list-group-flush">
+
+                            { 
+                                topProducts.map((product,index) => (
+                                    
+                                    <li key={index} className="list-group-item">
+                                     <h6>Title: &nbsp;&nbsp;
+                                        { product.title} 
+                                     </h6>
+                                     <h6>Total orders: &nbsp;&nbsp;
+                                      {  product.count}
+                                     </h6>
+                                    </li>
+                                ))
+                                
+                              }
+ 
+                            </ul>
+                        </div>
+                    </div>
+
+
+
                 </div>
             </div>
      );
