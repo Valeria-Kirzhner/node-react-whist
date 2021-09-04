717ef8d3707a96630166d28e077bbb9ea0a22fc3
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-09-05 00:07:26 +0300

    added another property to the query to get the number of orders count

--- routes/stats.js
+++ routes/stats.js
@@ -5,28 +5,33 @@ const _ = require("lodash");
 const mongoose = require('mongoose');
 
 // get top-5-most-selling-products 
+
 router.get('/top-products', async ( req, res ) => {
   try {
-const test =   await   Order.aggregate([
+
+      const test = await Order.aggregate([
       {
         $unwind: '$orderItems'
       },
-        {   $group: {_id : "$orderItems._id" , 
+        {   $group: {
+                    _id : "$orderItems._id" , 
                      total: {$sum: "$orderItems.qty"}, 
-                     title: { $first: "$orderItems.title"}
-                    }},
-        { $limit : 5 }
-     
-    ]).sort({total :-1})
+                     title: { $first: "$orderItems.title"},
+                     count: { $sum: 1 }
+                    }
+                  },
+                    { $limit : 5 } ])
+                    .sort({total :-1})
 
         res.send(test);
-        
+       
  } catch ( error ) {
-
      console.error(error);
      res.status(500).send("Server error");
  }
-
 }); 
+
+
+
 
 module.exports = router;
