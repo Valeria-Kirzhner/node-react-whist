63de43047b6574c108e61cacaf3a377a4ef96b4a
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-09-04 22:16:23 +0300

    created api to top-products

---
+++ routes/stats.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const { Order } = require("../models/order");
+const _ = require("lodash");
+const mongoose = require('mongoose');
+
+// get top-5-most-selling-products 
+router.get('/top-products', async ( req, res ) => {
+  try {
+const test =   await   Order.aggregate([
+      {
+        $unwind: '$orderItems'
+      },
+        {   $group: {_id : "$orderItems._id" , 
+                     total: {$sum: "$orderItems.qty"}, 
+                     title: { $first: "$orderItems.title"}
+                    }},
+        { $limit : 5 }
+     
+    ]).sort({total :-1})
+
+        res.send(test);
+        
+ } catch ( error ) {
+
+     console.error(error);
+     res.status(500).send("Server error");
+ }
+
+}); 
+
+module.exports = router;
