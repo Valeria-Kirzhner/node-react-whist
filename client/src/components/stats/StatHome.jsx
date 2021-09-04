cb34f9795b1db9f7ed0bef6d031c173013ddbc08
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-09-04 22:26:35 +0300

    added statsHome component

---
+++ client/src/components/stats/StatHome.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const StatHome = () => {
+
+    return ( 
+        <div className="col-md-6 col-lg-4 mt-3">
+            <div className="container">
+
+            </div>
+        </div>
+
+     );
+}
+ 
+export default StatHome;
