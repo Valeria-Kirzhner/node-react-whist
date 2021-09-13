8817914550b7a43058362af9bac32df289c790c5
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2021-09-13 12:36:34 +0300

    added sizing to image Product.

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -4,9 +4,9 @@ const Product = ({ product , onAddToCart }) => {
 
     return ( 
         <div className="col-md-6 col-lg-4 mt-3">
-            <div className="container">
+            <div className="container" >
                 <div className="card" style={{width: "18rem"}}>
-                    <img className="card-img-top" width="100" src={product.imageUrl} alt={product.title} />
+                    <img className="card-img-top" src={product.imageUrl} alt={product.title} style={{height: 300, width:250, display:'block', margin: 'auto'}}/>
                         <div className="card-body">
                             <h5 className="card-title">{product.title}</h5>
                             <p className="card-text">{product.description}</p>
